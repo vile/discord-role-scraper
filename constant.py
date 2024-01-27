4ac1ecdc394f76ae53fb10a9facaee4e311cbad1
Vile <111662603+vile@users.noreply.github.com>
2024-01-27 13:05:39 -0500

    fix: update constant.py
    - set discord api version in one place
    - update script version number
    - update script author
    - update request headers to mimic latest discord PTB client release

--- constant.py
+++ constant.py
@@ -4,20 +4,21 @@ import re
 from re import Pattern
 from typing import Final
 
+DISCORD_API_VERSION: Final[str] = "v9"
 DISCORD_API_BASE: Final[str] = "https://discord.com"
-DISCORD_API_AT_ME: Final[str] = f"{DISCORD_API_BASE}/api/v9/users/@me"
-DISCORD_API_GUILD: Final[str] = f"{DISCORD_API_BASE}/api/v9/guilds"
+DISCORD_API_AT_ME: Final[str] = f"{DISCORD_API_BASE}/api/{DISCORD_API_VERSION}/users/@me"
+DISCORD_API_GUILD: Final[str] = f"{DISCORD_API_BASE}/api/{DISCORD_API_VERSION}/guilds"
 
-VERSION_NUMBER: Final[str] = "2.0"
-SCRIPT_AUTHOR: Final[str] = "0xFantasy"
+VERSION_NUMBER: Final[str] = "3.0"
+SCRIPT_AUTHOR: Final[str] = "https://github.com/Vile"
 
 REQUEST_HEADERS: Final[dict] = {
     "accept": "*/*",
-    "accept-language": "en-US,en;q=0.6",
+    "accept-language": "en-US",
     "origin": "https://discord.com",
     "referer": "https://discord.com/channels/@me",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-    "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzExNC4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTE0LjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjIwNjY1MywiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0=",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.1054 Chrome/108.0.5359.215 Electron/22.3.26 Safari/537.36",
+    "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJwdGIiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC4xMDU0Iiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDUiLCJvc19hcmNoIjoieDY0IiwiYXBwX2FyY2giOiJpYTMyIiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV09XNjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIGRpc2NvcmQvMS4wLjEwNTQgQ2hyb21lLzEwOC4wLjUzNTkuMjE1IEVsZWN0cm9uLzIyLjMuMjYgU2FmYXJpLzUzNy4zNiIsImJyb3dzZXJfdmVyc2lvbiI6IjIyLjMuMjYiLCJjbGllbnRfYnVpbGRfbnVtYmVyIjoyNjE5NzMsIm5hdGl2ZV9idWlsZF9udW1iZXIiOjQzMTQzLCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==",
 }
 
 ANSI_ESCAPE: Final[Pattern] = re.compile(r"(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]")
