3e9dd5251e151eb83ca87f25929be7445166e7e9
Vile <111662603+vile@users.noreply.github.com>
2024-05-14 10:58:20 -0400

    fix: use closure instead of weird caching

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from colorama import Fore
 
 import config
@@ -9,43 +11,46 @@ import utils.guild as guild
 from utils.misc import clear
 
 
-def main(_cache: dict = {}) -> None:
-    clear()
-    print(f"{Fore.RED}Discord Role Scraper v{constant.VERSION_NUMBER} | {constant.SCRIPT_AUTHOR}{Fore.RESET}\n")  # fmt: skip
+def main() -> Callable[[], None]:
+    token: str = ""
 
-    while True:
-        if "token" not in _cache:
-            token: str = input(f"{Fore.RED}Token: {Fore.RESET}").strip("'\"")
-            if not account.check_token_is_valid(token):
-                print(f"{Fore.RED}[!] Your token seems to be invalid, make sure you are copying your FULL token without changing it and that your account is not terminated or locked{Fore.RESET}")  # fmt: skip
+    def run() -> None:
+        clear()
+        print(f"{Fore.RED}Discord Role Scraper v{constant.VERSION_NUMBER} | {constant.SCRIPT_AUTHOR}{Fore.RESET}\n")  # fmt: skip
+
+        nonlocal token
+
+        while True:
+            if token == "":
+                token = input(f"{Fore.RED}Token: {Fore.RESET}").strip("'\"")
+                if not account.check_token_is_valid(token):
+                    print(f"{Fore.RED}[!] Your token seems to be invalid, make sure you are copying your FULL token without changing it and that your account is not terminated or locked{Fore.RESET}")  # fmt: skip
+                    continue
+
+            server_id: int = input(f"{Fore.RED}Server ID: {Fore.RESET}")
+            if not guild.check_server_id_is_valid(server_id):
+                print(f"{Fore.RED}[!] Your server ID seems to be invalid, make sure you are copying the FULL numerical server ID without changing it{Fore.RESET}")  # fmt: skip
                 continue
+            break
 
-            _cache["token"] = token
-        else:
-            token: str = _cache["token"]
+        if config.SCRAPE_GUILD_INFO:
+            guild_info: dict = guild.scrape_guild_info(token, server_id)
+            display.display_guild_info(guild_info)
 
-        server_id: int = input(f"{Fore.RED}Server ID: {Fore.RESET}")
-        if not guild.check_server_id_is_valid(server_id):
-            print(f"{Fore.RED}[!] Your server ID seems to be invalid, make sure you are copying the FULL numerical server ID without changing it{Fore.RESET}")  # fmt: skip
-            continue
-        break
+        if config.SCRAPE_PERMISSION_INFO:
+            guild_roles: list = guild.scrape_guild_roles(token, server_id)
+            guild_formatted: str = display.display_guild_roles(guild_roles)
+            print(guild_formatted)
 
-    if config.SCRAPE_GUILD_INFO:
-        guild_info: dict = guild.scrape_guild_info(token, server_id)
-        display.display_guild_info(guild_info)
+            if config.EXPORT_RESULTS:
+                export.export_scrape_to_file(guild_formatted, server_id)
 
-    if config.SCRAPE_PERMISSION_INFO:
-        guild_roles: list = guild.scrape_guild_roles(token, server_id)
-        guild_formatted: str = display.display_guild_roles(guild_roles)
-        print(guild_formatted)
+        scrape_again: str = input(f"{Fore.YELLOW}[?] Scrape another server? (y/n): {Fore.RESET}").lower()  # fmt: skip
+        if "y" in scrape_again:
+            run()
 
-        if config.EXPORT_RESULTS:
-            export.export_scrape_to_file(guild_formatted, server_id)
-
-    scrape_again: str = input(f"{Fore.YELLOW}[?] Scrape another server? (y/n): {Fore.RESET}").lower()  # fmt: skip
-    if "y" in scrape_again:
-        main()
+    return run
 
 
 if __name__ == "__main__":
-    main()
+    main()()
