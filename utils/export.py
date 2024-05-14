73732a24e0cf00523c8b5f1f926c93c240b82342
Vile <111662603+vile@users.noreply.github.com>
2024-05-14 16:07:46 -0400

    fix: ensure export path exists
    - handle edge case where /export path doesnt exist

--- utils/export.py
+++ utils/export.py
@@ -1,8 +1,15 @@
+import os
 import time
 
 import utils.constant as constant
 
 
 def export_scrape_to_file(table_data: str, server_id: int) -> None:
+    if not os.path.exists("./export"):
+        try:
+            os.mkdir("./export")
+        except Exception:
+            pass
+
     with open(f"./export/{server_id}_{time.time()}_roles.txt", "w") as handle:
         handle.write(constant.ANSI_ESCAPE.sub(r"", table_data))
