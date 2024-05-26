65e9b2f87db4a50fc3eb5ba4316df429f9a969ef
Vile <111662603+vile@users.noreply.github.com>
2024-05-26 12:17:02 -0400

    fix: prevent writing export file if export folder doesnt exist and cant be made

--- utils/export.py
+++ utils/export.py
@@ -9,7 +9,7 @@ def export_scrape_to_file(table_data: str, server_id: int) -> None:
         try:
             os.mkdir("./export")
         except Exception:
-            pass
+            return
 
     with open(f"./export/{server_id}_{time.time()}_roles.txt", "w") as handle:
         handle.write(constant.ANSI_ESCAPE.sub(r"", table_data))
